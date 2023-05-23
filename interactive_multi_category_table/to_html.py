3ce360faae07d32da91c36935ca535adf19940f8
Xiang CHEN <xiangchen.ai@outlook.com>
2023-05-23 10:43:47 +0800

    bug fix

--- interactive_multi_category_table/to_html.py
+++ interactive_multi_category_table/to_html.py
@@ -5,7 +5,7 @@ def json_to_html(json_file_path, encoding='utf-8'):
     category_list, tag_list, category_dict, contents = parse_json(json_file_path, encoding)
     title = os.path.split(json_file_path)[-1][:-5]
 
-    with open("template.html", "r") as f:
+    with open(os.path.join(os.path.split(__file__)[0], "template.html"), "r") as f:
         html = f.read()
 
     html = html.replace("__title__", title)
@@ -14,7 +14,7 @@ def json_to_html(json_file_path, encoding='utf-8'):
     html = html.replace("__category_dict__", str(category_dict))
     html = html.replace("__contents__", str(contents))
 
-    with open(json_file_path+".html", "w") as f:
+    with open(json_file_path+".html", "w", encoding="utf-8") as f:
         f.write(html)
 
 if __name__=="__main__":
